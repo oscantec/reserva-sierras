b65cc1eb8b6f89354dfecacb49309217b328d239
MAC MINI M2 <macminim2@Mac-mini-de-MAC.local>
2026-09-14 13:36:21 -0500

    Revert "feat: style guide top section like homepage"
    This reverts commit 7d7f28371807075c073d12474d431fed7a8f2772.

--- src/pages/Guia.jsx
+++ src/pages/Guia.jsx
@@ -53,7 +53,7 @@ export default function Guia() {
     }, [])
 
     return (
-        <div className="guide-top-page">
+        <div id="unstyled-guide">
             <Navbar />
 
             <main className="guide-main">
